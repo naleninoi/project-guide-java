dd2823668423cdcaefee0977bd5419118326c027
Viktor Kochetov-Arhipov <naleninoi@gmail.com>
2023-09-16 14:49:14 +0300

    ex 2-8

---
+++ src/main/java/com/project_guide/chapter_02/BankStatementProcessor.java
@@ -0,0 +1,41 @@
+package main.java.com.project_guide.chapter_02;
+
+import java.time.Month;
+import java.util.List;
+
+public class BankStatementProcessor {
+
+    private final List<BankTransaction> bankTransactions;
+
+    public BankStatementProcessor(final List<BankTransaction> bankTransactions) {
+        this.bankTransactions = bankTransactions;
+    }
+
+    public double calculateTotalAmount() {
+        double total = 0d;
+        for (final BankTransaction bankTransaction : bankTransactions) {
+            total += bankTransaction.amount();
+        }
+        return total;
+    }
+
+    public double calculateTotalInMonth(final Month month) {
+        double total = 0d;
+        for (final BankTransaction bankTransaction : bankTransactions) {
+            if (bankTransaction.date().getMonth() == month) {
+                total += bankTransaction.amount();
+            }
+        }
+        return total;
+    }
+
+    public double calculateTotalInCategory(final String category) {
+        double total = 0d;
+        for (final BankTransaction bankTransaction : bankTransactions) {
+            if (bankTransaction.description().equalsIgnoreCase(category)) {
+                total += bankTransaction.amount();
+            }
+        }
+        return total;
+    }
+}
