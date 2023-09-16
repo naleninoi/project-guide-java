00eac655409b21ff8de37da45a4dc3812ab9ece8
Viktor Kochetov-Arhipov <naleninoi@gmail.com>
2023-09-16 14:36:49 +0300

    ex 2-6

---
+++ src/main/java/com/project_guide/chapter_02/BankTransaction.java
@@ -0,0 +1,31 @@
+package main.java.com.project_guide.chapter_02;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+public record BankTransaction(LocalDate date, double amount, String description) {
+
+    @Override
+    public String toString() {
+        return "BankTransaction{" +
+                "date=" + date +
+                ", amount=" + amount +
+                ", description='" + description + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        BankTransaction that = (BankTransaction) o;
+        return Double.compare(that.amount, amount) == 0 &&
+                date.equals(that.date) &&
+                description.equals(that.description);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(date, amount, description);
+    }
+}
